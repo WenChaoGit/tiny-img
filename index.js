d92b6f7f1ca9029919278302fc1738eca3d09504
WenChaoGit <1031229910@qq.com>
2019-04-30 07:47:07 +0800

    a

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
 const tinify = require("tinify");
 const shell = require('shelljs');
-const fs = require("fs-extra");
 const path = require('path');
 const {api_key,src_path,out_path} = require('./config')
 const {tinyImg,getTinyParams} =require('./lib/image')
-//配置
-
-tinify.key = api_key
 //压缩
 shell.ls(src_path).forEach(async item => {
   // 判断文件类型，图片的保留
