2d2fd0a1c0806048bd34ddd2fcaac353c2c54df4
WenChaoGit <1031229910@qq.com>
2019-04-30 07:17:58 +0800

    a

--- index.js
+++ index.js
@@ -9,26 +9,30 @@ tinify.key = api_key
 //压缩
 shell.ls(src_path).forEach(async item => {
   // 判断文件类型，图片的保留
-  let extname = path.extname(item);
-  let isAllowedExt = ['.png','.jpeg','.jpg'].find(item => item ==extname);
-  if(isAllowedExt){
-      let params = getTinyParams(item)
-      tinyImg(params)
-  }
+  shell.ls(`${src_path}/${item}`).forEach(itemChild => {
+    
+    let extname = path.extname(itemChild);
+    let isAllowedExt = ['.png','.jpeg','.jpg'].find(item => item ==extname);
+    if(isAllowedExt){
+        let params = getTinyParams(item,itemChild)
+        // console.log(params);return;
+        tinyImg(params)
+    }
+  })
+  
  
 })
 
 function tinyImg({src_file,out_file}) {
   const source =  tinify.fromFile(src_file);
-  await fs.mkdir(`${out_file}`,{recursive:true});
   source.toFile(`${out_file}`,err => {
     if(err) throw err;
-    console.log(`${item} 压缩成功`);
+    console.log(`${out_file} 压缩成功`);
   });  
 }
 
-function getTinyParams(item){
-  let src_file = `${src_path}/${item}`
-  let out_file = `${out_path}/${item}`;
+function getTinyParams(item,itemChild){
+  let src_file = `${src_path}/${item}/${itemChild}`
+  let out_file = `${out_path}/${item}/test${itemChild}`;
   return {src_file,out_file}
 }
